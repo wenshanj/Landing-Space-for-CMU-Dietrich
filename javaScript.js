0ee74f302508b13d03f2ae02b408a45656c3d40f
Wen Shan Jiang <wenshanj@andrew.cmu.edu>
2020-12-28 04:00:42 -0500

    init

---
+++ javaScript.js
@@ -0,0 +1,15 @@
+mybutton = document.getElementById("backToTopBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function backToTopFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+}
